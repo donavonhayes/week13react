549519663e9b61ae246b842836e080ca66b37141
donavonhayes <131027014+donavonhayes@users.noreply.github.com>
2023-07-05 21:06:22 -0500

    8 commit

---
+++ src/components/main-page.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import NavBar from './nav-bar'
+import LoginForm from './login-form'
+
+
+export default class MainPage extends Component {
+ render() {
+   return (
+     <>
+       <NavBar />
+       <LoginForm />
+     </>
+   )
+ }
+}
